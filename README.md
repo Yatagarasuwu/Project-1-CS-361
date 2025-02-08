b5c9322bb4f24d1bb7a889428a24e5c31f2b9e63
Sabastian Leeper <sabastianleeper@u.boisestate.edu>
2025-02-07 19:16:53 -0700

    Basic Setup for the Project.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project-1-CS-361
+This is a temporary READMe.md for now.
---
+++ fa/dfa/DFAState.java
@@ -0,0 +1,14 @@
+/*
+ * Adding a block-comment so I don't forget to later. 
+ * @Author 
+ * @Date
+ * @Description
+ */
+package fa.dfa;
+
+public class DFAState extends fa.State {
+    
+    public DFAState(String name) {
+        super(name);
+    }   
+}
